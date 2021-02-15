aa3c7415dc79c11b7a591a66fac48510831785ea
arutyunyan <arutyunyan@bolid.ru>
2021-02-15 15:34:41 +0300

    added some comments

--- scratch.php
+++ scratch.php
@@ -5,9 +5,14 @@ require_once('vendor/autoload.php');
 use sagittaracc\core\File;
 
 (new File('scratch.php'))->onChange(function(){
-  // Time update has changed
+
+  // return if time update has changed
   return $this->timeUpdateTrigger();
+
 }, function(){
+
+  // this runs if event has been triggered
   echo 'Start tracking...';
   $this->track();
+
 });
